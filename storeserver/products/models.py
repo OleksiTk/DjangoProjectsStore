791e37b194e412021471db073402cdb59fa19d9c
OleksiTK1 <oleksiy_t@yr-solutions.team>
2026-05-16 21:17:47 +0300

    add new info course

--- storeserver/products/models.py
+++ storeserver/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    description = models.TextField(null=True, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quitantity = models.PositiveBigIntegerField(default=0)
+    image = models.ImageField(upload_to='product_images/')
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, related_name='products')
--- storeserver/products/views.py
+++ storeserver/products/views.py
@@ -3,8 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        "Title": "Products",
+        'is_active': False,
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request,):
-    return render(request, 'products/products.html')
+    context = {
+        "Title": "Products",
+        "products": [
+            {
+                "image" : "/static/vendor/img/products/Adidas-hoodie.png",
+                "name": "Худи черного цвета с монограммами adidas Originals",
+                "description": " Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.",
+                "price": '6 090,00 грн.',
+            },
+            {
+                    "image" : "/static/vendor/img/products/Adidas-hoodie.png",
+                    "name": "Худи черного цвета с монограммами adidas Originals",
+                    "description": " Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.",
+                    "price": '6 090,00 грн.',
+            },
+            {
+                    "image" : "/static/vendor/img/products/Adidas-hoodie.png",
+                    "name": "Худи черного цвета с монограммами adidas Originals",
+                    "description": " Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.",
+                    "price": '6 090,00 грн.',
+            }
+        ]
+    }
+    return render(request, 'products/products.html',context)
